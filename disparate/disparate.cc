5bbae632d9a6e31bccf1a77d3a3096367cc198aa
Qbell <Qbell@aws_workspace>
2023-02-20 20:08:09 -0600

    with int, the "sum" number is not rounded. WIth float, the highest possible value is so high that  the computer just rounds sum to teh nearest representable value due to precision.

--- disparate/disparate.cc
+++ disparate/disparate.cc
@@ -1,5 +1,7 @@
 #include <iomanip>
 #include <iostream>
+#include <climits>
+#include <cmath>
 
 using namespace std;
 
@@ -10,12 +12,14 @@ int main ()
     // CREATE THREE int32_t VALUES //
     // (big, small, sum)           //
     /////////////////////////////////
-
+    int32_t big= pow(10,8);
+    int32_t small= 1;
+    int32_t sum= big+small;
 
     cout << "int32_t:" << endl;
-    cout << "  big   = " << big   << endl;
-    cout << "  small = " << small << endl;
-    cout << "  sum   = " << sum   << endl;
+    cout << "  big   = " << big  << endl;
+    cout << "  small = " << small<< endl;
+    cout << "  sum   = " << sum  << endl;
   }
 
   {
@@ -23,12 +27,11 @@ int main ()
     // CREATE THREE float VALUES //
     // (big, small, sum)         //
     ///////////////////////////////
-
-
+float big= pow(10,8), small =1;
+float sum = big+small;
     cout << "float:" << endl;
     cout << "  big   = " << fixed << setprecision(1) << big   << endl;
     cout << "  small = " << fixed << setprecision(1) << small << endl;
     cout << "  sum   = " << fixed << setprecision(1) << sum   << endl;
-  }
+  } 
 }
-
