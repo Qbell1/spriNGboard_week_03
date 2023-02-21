e489b896adb5e4180296b19b0c94c6d0d0729158
Qbell <Qbell@aws_workspace>
2023-02-20 21:47:41 -0600

    notes in code.

--- bit_values/unsigned.cc
+++ bit_values/unsigned.cc
@@ -11,36 +11,42 @@ int main ()
 
   ///////////////////////////////////
   // SET THE APPROPRIATE BITS HERE //
-  ///////////////////////////////////
+ 
+  x=x|(1<<2);
+  x=x|(1<<3);
+  x=x|(1<<5);
+  x=x|(1<<7);
+  x=x|(1<<9);
+  x=x|(1<<11);
+  x=x|(1<<13);
+  x=x|(1<<17);
+  x=x|(1<<19);
+  x=x|(1<<21);
+  x=x|(1<<23);
+  x=x|(1<<29);
+  x=x|(1<<31); 
 
   cout << "after setting bits:" << endl;
   cout << "  x = " << x << endl;
 
-  ///////////////////////
-  // TOGGLE BIT 3 HERE //
-  ///////////////////////
-
+    x=x^(1<<3);     // This subtracts 8
   cout << "after first bit 3 toggle:" << endl;
   cout << "  x = " << x << endl;
 
-  //////////////////////////////////////
-  // REPEAT YOUR PREVIOUS ACTION HERE //
-  //////////////////////////////////////
-
+      x=x^(1<<3);       // This toggles the 0 back to 1 and adds 8
   cout << "after second bit 3 toggle:" << endl;
   cout << "  x = " << x << endl;
 
-  //////////////////////
-  // CLEAR BIT 7 HERE //
-  //////////////////////
-
+    x &= ~(1<<7);         // This subtracts 128 by changing bit 7 to a 0
   cout << "after clearing bit 7:" << endl;
   cout << "  x = " << x << endl;
 
   ///////////////////////////
   // CLEAR BITS 16-31 HERE //
   ///////////////////////////
-
+for (int f=16; f<=31; f++){
+    x &= ~(1<<f);       // Subtracts the value of each bit from the total by changing the bit 0
+}
   cout << "after clearing bits 16-31:" << endl;
   cout << "  x = " << x << endl;
 }
